47c4e172c8ced72d7e818606be50cdcf2a5d5ec0
ohheyimjen <jendority@gmail.com>
2019-04-19 10:31:10 -0700

    Completed first draft of SmurfForm.js

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -15,6 +15,45 @@ handleInputChange = e => {
     this.setState({ [event.target.name]: event.target.value });
 };
 
-handleAddSmurf = e => {
-    const {name, age, }
-}
+addSmurf = event => {
+    event.preventDefault();
+    const smurf = this.state
+    axios
+    .post('http://localhost:3333/smurfs/', smurf)
+    .then(response => {
+      this.props.updateSmurfs(response.data);
+      this.props.history.push('/')
+    })
+    .catch(error => console.log(error));
+
+    render() {
+        return (
+          <div className="SmurfForm">
+            <form onSubmit={this.addSmurf}>
+              <input
+                onChange={this.handleInputChange}
+                placeholder="name"
+                value={this.state.name}
+                name="name"
+              />
+              <input
+                onChange={this.handleInputChange}
+                placeholder="age"
+                value={this.state.age}
+                name="age"
+              />
+              <input
+                onChange={this.handleInputChange}
+                placeholder="height"
+                value={this.state.height}
+                name="height"
+              />
+              <button type="submit">Add to the village</button>
+            </form>
+          </div>
+        );
+      }
+    }
+    
+    export default SmurfForm;
+    
